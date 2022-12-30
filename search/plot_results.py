5a09a6835dcb8dcc0fa9348aa3cdd0b4e15b7f41
Aryaman Raina <105023486+araina25@users.noreply.github.com>
2022-12-30 10:03:41 -0700

    Add files via upload

---
+++ search/plot_results.py
@@ -0,0 +1,38 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+
+class PlotResults:
+    """
+    Class to plot the results. 
+    """
+    def plot_results(self, data1, data2, label1, label2, filename):
+        """
+        This method receives two lists of data point (data1 and data2) and plots
+        a scatter plot with the information. The lists store statistics about individual search 
+        problems such as the number of nodes a search algorithm needs to expand to solve the problem.
+
+        The function assumes that data1 and data2 have the same size. 
+
+        label1 and label2 are the labels of the axes of the scatter plot. 
+        
+        filename is the name of the file in which the plot will be saved.
+        """
+        _, ax = plt.subplots()
+        ax.scatter(data1, data2, s=100, c="g", alpha=0.5, cmap=plt.cm.coolwarm, zorder=10)
+    
+        lims = [
+        np.min([ax.get_xlim(), ax.get_ylim()]),  # min of both axes
+        np.max([ax.get_xlim(), ax.get_ylim()]),  # max of both axes
+        ]
+    
+        ax.plot(lims, lims, 'k-', alpha=0.75, zorder=0)
+        ax.set_aspect('equal')
+        ax.set_xlim(lims)
+        ax.set_ylim(lims)
+        plt.xlabel(label1)
+        plt.ylabel(label2)
+        plt.grid()
+        plt.savefig(filename)
+        #plt.show()
